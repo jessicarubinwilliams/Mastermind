be2161779f9e720dd97abbd753fb19fa7ef74bbb
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2025-08-23 15:38:52 -0700

    Set up FE Client App: Vue3 + Vite w/Axios, SCSS, ESLint, and Prettier

---
+++ src/Mastermind.Client/prettier.config.mjs
@@ -0,0 +1,9 @@
+export default {
+	useTabs: true,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	vueIndentScriptAndStyle: true,
+	endOfLine: 'lf',
+};
---
+++ src/Mastermind.Client/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+	<main class="app-container">
+		<h1>Mastermind</h1>
+
+		<HealthCheck />
+	</main>
+</template>
+
+<script setup lang="ts">
+	import { ref } from 'vue'
+	import HealthCheck from '@/components/HealthCheck.vue'
+</script>
